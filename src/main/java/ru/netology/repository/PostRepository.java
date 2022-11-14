ffa087b04e8ae00b0d954be86abf5d96df153501
KristinaKac <kachalkovakristina@mailru>
2022-11-14 19:20:17 +0300

    application context

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,12 +1,13 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public class PostRepository {
   public List<Post> list = new ArrayList<>();
   Optional<Post> opt;
